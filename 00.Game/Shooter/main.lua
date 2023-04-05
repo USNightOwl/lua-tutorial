307dd16c8b9ac653fbc0b739a52fb6229cb426c2
haonhat0703 <haonhat2729@gmail.com>
2023-04-05 23:50:36 +0700

    split to many file

--- 00.Game/Shooter/main.lua
+++ 00.Game/Shooter/main.lua
@@ -1,80 +1,73 @@
 require("./const")
+require('./Player')
+require('./Enemy')
+require('./Bullet')
 
-function love.load()
+local function loadGame()
   LoadPlayer()
   LoadBullets()
+  LoadEnemy()
+  Score = 0
+  Game = true
+end
+
+function love.load()
+  loadGame()
 end
 
 function love.update(dt)
-  if love.keyboard.isDown("escape") then
-    love.event.quit()
+  if Game then
+    PlayerControl(dt)
+    UpdateBullet(dt)
+    UpdateEnemies(dt)
   end
 
-  PlayerControl(dt)
-  UpdateBullet(dt)
+  if TouchPlayer() then
+    Game = false
+  end
 end
 
 function love.draw()
-  DrawBullets()
-  DrawPlayer()
-end
-
-function PlayerControl(dt)
-  if love.keyboard.isDown("d") then
-    Player.x = Player.x + Player.speed * dt
-  elseif love.keyboard.isDown("a") then
-    Player.x = Player.x - Player.speed * dt
+  local x, y, R, G, B, font = 0, 0, 1, 1, 1, 30
+  if Game then
+    DrawBullets()
+    DrawPlayer()
+    DrawEnemy()
+  else
+    local gameFont = love.graphics.newFont(80)
+    love.graphics.setBackgroundColor(0, 0, 0)
+    love.graphics.setFont(gameFont)
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.print("YOU LOSE", WIDTH / 2 - 200, HEIGHT / 5 - 50)
+    love.graphics.setColor(1, 1, 1)
+    gameFont = love.graphics.newFont(28)
+    love.graphics.setFont(gameFont)
+    love.graphics.print("Press r to play again", WIDTH / 2 - 130, HEIGHT - 250)
+    x, y = WIDTH / 2 - 100, HEIGHT / 5 + 120
+    R, G, B = 0, 0, 1
+    font = 50
   end
 
-  if love.keyboard.isDown("w") then
-    Player.y = Player.y - Player.speed * dt
-  elseif love.keyboard.isDown("s") then
-    Player.y = Player.y + Player.speed * dt
-  end
+  local gameFont = love.graphics.newFont(font)
+  love.graphics.setColor(R, G, B)
+  love.graphics.setFont(gameFont)
+  love.graphics.print("SCORE:\n    " .. tostring(Score), x, y)
 end
 
 function love.mousepressed(x, y, button, istouch)
-  CreateBullet(x, y, button)
-end
-
-function UpdateBullet(dt)
-  if #Bullets <= 0 then
-    return
-  end
-
-  for i = #Bullets, 1, -1 do
-    Bullets[i].x = Bullets[i].x + Bullets[i].dx * dt
-    Bullets[i].y = Bullets[i].y + Bullets[i].dy * dt
-
-    if Bullets[i].x <= 0 or Bullets[i].x >= WIDTH or Bullets[i].y <= 0 or Bullets[i].y >= HEIGHT then
-      table.remove(Bullets, i)
-    end
+  if Game then
+    CreateBullet(x, y, button)
   end
 end
 
-function DrawBullets()
-  for i = 1, #Bullets do
-    love.graphics.setColor(1, 0, 0)
-    love.graphics.circle("fill", Bullets[i].x, Bullets[i].y, 5)
+function love.keypressed(key, scancode, isrepeat)
+  if key == "escape" then
+    love.event.quit()
   end
-end
-
-function DrawPlayer()
-  local angle = math.atan2((love.mouse.getY() - Player.y), (love.mouse.getX() - Player.x))
-
-  love.graphics.translate(Player.x - 5 + 5, Player.y)
-  love.graphics.rotate(angle - math.pi / 2)
-  love.graphics.translate(-Player.x, -Player.y)
-
-  love.graphics.setColor(1, 1, 0)
-  love.graphics.rectangle("fill", Player.x - 7, Player.y, 14, 40)
-  love.graphics.setColor(0, 1, 0)
-  love.graphics.circle("fill", Player.x, Player.y, 20)
-  love.graphics.setColor(0, 0, 0)
-  love.graphics.circle("fill", Player.x + 10, Player.y + 5, 4)
-  love.graphics.circle("fill", Player.x - 10, Player.y + 5, 4)
-
-  love.graphics.translate(Player.x, Player.y)
-  love.graphics.rotate(-angle + math.pi / 2)
-  love.graphics.translate(-Player.x, -Player.y)
+  if key == "space" and Game then
+    CreateBullet(love.mouse.getX(), love.mouse.getY())
+  end
+  if key == "r" and Game == false then
+    loadGame()
+  end
 end
