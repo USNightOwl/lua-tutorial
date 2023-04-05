f4628e098cb32c8be611393e0833bf1d727975e6
haonhat0703 <haonhat2729@gmail.com>
2023-04-05 20:18:51 +0700

    main game file shooter simple with only circle

--- 00.Game/Shooter/main.lua
+++ 00.Game/Shooter/main.lua
@@ -1,14 +1,62 @@
+require("./const")
+
 function love.load()
-  Number = 0
-  love.window.setMode(1000, 720)
+  LoadPlayer()
+  LoadBullets()
 end
 
 function love.update(dt)
-  Number = Number + 1
+  if love.keyboard.isDown("escape") then
+    love.event.quit()
+  end
+
+  PlayerControl(dt)
+  UpdateBullet(dt)
 end
 
 function love.draw()
-  love.graphics.rectangle("line", 50, 100, 100, 400)
-  love.graphics.rectangle("fill", 100, 100, 100, 400)
-  love.graphics.rectangle("fill", 100, 100, 100, 400)
+  love.graphics.setColor(0, 1, 0)
+  love.graphics.circle("fill", Player.x, Player.y, 20)
+  DrawBullets()
+end
+
+function PlayerControl(dt)
+  if love.keyboard.isDown("d") then
+    Player.x = Player.x + Player.speed * dt
+  elseif love.keyboard.isDown("a") then
+    Player.x = Player.x - Player.speed * dt
+  end
+
+  if love.keyboard.isDown("w") then
+    Player.y = Player.y - Player.speed * dt
+  elseif love.keyboard.isDown("s") then
+    Player.y = Player.y + Player.speed * dt
+  end
+end
+
+function love.mousepressed(x, y, button, istouch)
+  CreateBullet(x, y, button)
+end
+
+function UpdateBullet(dt)
+  if #Bullets <= 0 then
+    return
+  end
+
+  for i = #Bullets, 1, -1 do
+    Bullets[i].x = Bullets[i].x + Bullets[i].dx * dt
+    Bullets[i].y = Bullets[i].y + Bullets[i].dy * dt
+
+    if Bullets[i].x <= 0 or Bullets[i].x >= WIDTH or Bullets[i].y <= 0 or Bullets[i].y >= HEIGHT then
+      table.remove(Bullets, i)
+    end
+  end
+end
+
+function DrawBullets()
+  for i = 1, #Bullets do
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", Bullets[i].x, Bullets[i].y, 5)
+    love.graphics.setColor(1, 1, 1)
+  end
 end
