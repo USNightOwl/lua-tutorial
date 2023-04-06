09a5a1d4dc80962fbebb7d729f7b48dc539c04ec
haonhat0703 <haonhat2729@gmail.com>
2023-04-06 17:37:11 +0700

    add sound background

--- 00.Game/Shooter/const.lua
+++ 00.Game/Shooter/const.lua
@@ -3,6 +3,8 @@ WIDTH = love.graphics.getWidth()
 MAX_LIFE = 3
 Game = true
 Score = 0
+ChangeBG = 10
+SoundBG = love.audio.newSource("Sounds/background.mp3", "stream")
 
 function IsCollision(x1, y1, r1, x2, y2, r2)
   local distance = math.sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
