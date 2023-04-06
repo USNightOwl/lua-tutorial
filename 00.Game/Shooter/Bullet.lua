1511c243e88ed315ceddf3ecca006ad99bfd1195
haonhat0703 <haonhat2729@gmail.com>
2023-04-06 17:38:46 +0700

    add graphic and sound

--- 00.Game/Shooter/Bullet.lua
+++ 00.Game/Shooter/Bullet.lua
@@ -1,7 +1,13 @@
+local bullet_img = love.graphics.newImage("/Graphics/bullet.png")
+local width, height = bullet_img:getDimensions()
+local soundCollision = love.audio.newSource("/Sounds/enemy_hit.wav", "static")
+local soundFire = love.audio.newSource("/Sounds/player_fire.wav", "static")
+soundFire:setVolume(0.5)
+
 function LoadBullets()
   Bullets = {}
   Bullets.speed = 250
-  Bullets.radius = 5
+  Bullets.radius = width / 2
 end
 
 function CreateBullet(x, y, button)
@@ -18,13 +24,26 @@ function CreateBullet(x, y, button)
   local bulletDx = Bullets.speed * math.cos(angle)
   local bulletDy = Bullets.speed * math.sin(angle)
 
-  table.insert(Bullets, { x = startX, y = startY, dx = bulletDx, dy = bulletDy })
+  soundFire:play()
+  table.insert(Bullets, { x = startX, y = startY, dx = bulletDx, dy = bulletDy, angle = angle })
 end
 
 function DrawBullets()
   for i = 1, #Bullets do
-    love.graphics.setColor(1, 0, 0)
-    love.graphics.circle("fill", Bullets[i].x, Bullets[i].y, Bullets.radius)
+    --love.graphics.setColor(1, 0, 0)
+    --love.graphics.circle("fill", Bullets[i].x, Bullets[i].y, Bullets.radius)
+    local angle = Bullets[i].angle
+    local x, y = Bullets[i].x, Bullets[i].y
+
+    love.graphics.translate(x, y)
+    love.graphics.rotate(angle + math.pi / 2)
+    love.graphics.translate(-x, -y)
+
+    love.graphics.draw(bullet_img, Bullets[i].x, Bullets[i].y, 0, 1, 1, width / 2, height / 2)
+
+    love.graphics.translate(x, y)
+    love.graphics.rotate(-angle - math.pi / 2)
+    love.graphics.translate(-x, -y)
   end
 end
 
@@ -55,6 +74,7 @@ function ChkCollision()
       if IsCollision(Bullets[i].x, Bullets[i].y, Bullets.radius, Enemies[j].x, Enemies[j].y, Enemies.radius) then
         table.remove(Bullets, i)
         Enemies[j].life = Enemies[j].life - 1
+        soundCollision:play()
         if Enemies[j].life <= 0 then
           table.remove(Enemies, j)
           Score = Score + 1
