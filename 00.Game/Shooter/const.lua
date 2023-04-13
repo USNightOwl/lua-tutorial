8a7b85741ec78f6ff91f2698b8e56cb928087987
haonhat0703 <haonhat2729@gmail.com>
2023-04-13 23:43:13 +0700

    add variable State player

--- 00.Game/Shooter/const.lua
+++ 00.Game/Shooter/const.lua
@@ -7,6 +7,8 @@ ChangeBG = 10
 SoundBG = love.audio.newSource("Sounds/background.mp3", "stream")
 Stop = false
 Runtime = 0
+State = 1
+StateTime = 0
 
 function IsCollision(x1, y1, r1, x2, y2, r2)
   local distance = math.sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
