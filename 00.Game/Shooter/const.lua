33b4de491e098846dda11a8204ff25dd40d41a0f
haonhat0703 <haonhat2729@gmail.com>
2023-04-13 21:35:56 +0700

    stop time

--- 00.Game/Shooter/const.lua
+++ 00.Game/Shooter/const.lua
@@ -5,6 +5,8 @@ Game = true
 Score = 0
 ChangeBG = 10
 SoundBG = love.audio.newSource("Sounds/background.mp3", "stream")
+Stop = false
+Runtime = 0
 
 function IsCollision(x1, y1, r1, x2, y2, r2)
   local distance = math.sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
